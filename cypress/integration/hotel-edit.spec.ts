90f1fe6989fb1f15edb01d6e9c2426cc45cf0372
EJMarrero <EJMarrero1989@gmail.com>
2020-09-12 12:04:02 +0100

    Adding CI

--- cypress/integration/hotel-edit.spec.ts
+++ cypress/integration/hotel-edit.spec.ts
@@ -27,6 +27,6 @@ describe('Hotel edit specs', () => {
 
     // Assert
     cy.wait('@load'); // TODO: Refactor
-    cy.findByText('Updated hotel two');
+    cy.findByText('Updated hotel aaaajajaja');
   });
 });
